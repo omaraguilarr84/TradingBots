7b35df7544a284fce4e12e85a4c73799bbdded74
Omar Aguilar Jr <omaraguilarjr@19.130.21.198.guest.wifi.dyn.clemson.edu>
2021-09-27 12:23:13 -0400

    updates

---
+++ bot.py
@@ -0,0 +1,27 @@
+import websocket, json, pprint
+
+SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+
+
+def on_open(ws):
+    print('opened connection')
+
+def on_close(ws):
+    print('closed connection')
+
+def on_message(ws, message):
+    print('received message')
+    print(message)
+    json_message = json.loads(message)
+    pprint.pprint(json_message)
+
+    candle = json.message['k']
+
+    is_candle_closed = candle['x']
+    close = candle['c']
+
+    if is_candle_closed:
+        print("candle closed at {}".format(close))
+
+ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
+ws.run_forever()
