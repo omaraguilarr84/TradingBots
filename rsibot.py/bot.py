fa1e32325fcb7319b49026902949fe0b578bbde2
omaraguilarr84 <omar.hawks8@gmail.com>
2021-09-28 07:55:27 -0400

    updates

--- rsibot.py/bot.py
+++ rsibot.py/bot.py
@@ -1,8 +1,15 @@
-import websocket, json, pprint
+import websocket, json, pprint, talib, numpy
 
-SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+SOCKET = "wss://stream.binance.com:9443/ws/adausdt@kline_1m"
+RSI_PERIOD = 14
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
+TRADE_SYMBOL = 'ADAUSD'
+TRADE_QUANTITY = 3
 
-closes= []
+closes = []
+in_position = False
+
 def on_open(ws):
     print('opened connection')
 
@@ -22,6 +29,29 @@ def on_message(ws, message):
 
     if is_candle_closed:
         print("candle closed at {}".format(close))
+        closes.append(float(close))
+        print("closes")
+        print(closes)
+
+        if len(closes) > RSI_PERIOD:
+            np_closes = numpy.array(closes)
+            rsi = talib.RSI(np_closes, RSI_PERIOD)
+            print("all rsis calculated so far")
+            print(rsi)
+            last_rsi = rsi[-1]
+            print("the current rsi is {}".format(last_rsi))
+
+            if last_rsi > RSI_OVERBOUGHT:
+                if in_position:
+                    print("Overbought! Sell!")
+                else:
+                    print("It is overbought, but none owned.")
+
+            if last_rsi < RSI_OVERSOLD:
+                if in_position:
+                    print("It is oversold, but already owned.")
+                else:
+                    print("Oversold! Buy!")
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
