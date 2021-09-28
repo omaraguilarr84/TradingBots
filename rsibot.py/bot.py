2da13cdeb5b3987c7b1eaf2d42dacafbc145b216
omaraguilarr84 <omar.hawks8@gmail.com>
2021-09-28 12:06:04 -0400

    Final ADA

--- rsibot.py/bot.py
+++ rsibot.py/bot.py
@@ -1,4 +1,5 @@
 import websocket, json, pprint, talib, numpy
+import config
 from binance.client import Client
 from binance.enums import *
 
@@ -7,10 +8,25 @@ RSI_PERIOD = 14
 RSI_OVERBOUGHT = 70
 RSI_OVERSOLD = 30
 TRADE_SYMBOL = 'ADAUSD'
-TRADE_QUANTITY = 3
+TRADE_QUANTITY = 5
 
 closes = []
 in_position = False
+
+client = Client(config.API_KEY, config.API_SECRET, tld='us')
+
+def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
+    try: 
+        print("sending order")
+        order = client.create_order(symbol=symbol,
+        side = side,
+        type = order_type,
+        quantity = quantity)
+        print(order)
+    except Exception as e:
+        return False
+
+    return True
 
 def on_open(ws):
     print('opened connection')
@@ -46,6 +62,9 @@ def on_message(ws, message):
             if last_rsi > RSI_OVERBOUGHT:
                 if in_position:
                     print("Overbought! Sell!")
+                    order_succeeded = order(SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL)
+                    if order_succeeded:
+                        in_position = False
                 else:
                     print("It is overbought, but none owned.")
 
@@ -54,6 +73,9 @@ def on_message(ws, message):
                     print("It is oversold, but already owned.")
                 else:
                     print("Oversold! Buy!")
+                    order_succeeded = order(SIDE_BUY, TRADE_QUANTITY,TRADE_SYMBOL)
+                    if order_succeeded:
+                        in_position = True
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
